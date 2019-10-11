2b3b0180bfe785c04826e0f06140e8d98f5f8d7b
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-10-12 00:06:28 +0100

    Install Sass devDependencies & create a src/assets/sass/main.scss file

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1 class="testFromMainScss">Hello, world!</h1>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
@@ -26,8 +26,10 @@ export default {
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style lang="scss" scoped>
+.home-links {
+  a {
+    margin-right: 1rem;
+  }
 }
 </style>
