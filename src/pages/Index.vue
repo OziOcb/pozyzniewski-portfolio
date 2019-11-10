17191c8c4ba239e147ad2ff4af6142a4a51d6770
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-11-10 10:47:16 +0000

    Clear the Index.vue file

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,6 @@
 <template>
   <Layout>
-    <div class="dummyDiv"></div>
-    <div class="smElement">scroll magic test</div>
-    <div class="dummyDiv"></div>
+    <h1 class="hello">{{ msg }}</h1>
   </Layout>
 </template>
 
@@ -11,36 +9,16 @@ export default {
   metaInfo: {
     title: "Hello, world!"
   },
-  mounted() {
-    // create a Tween
-    const testTween = new this.$GSAP.TimelineMax()
-    testTween
-      .from(".smElement", 1.5, { ease: this.$GSAP.Power2.easeOut, opacity: 0 })
-      .to(".smElement", 2, { ease: this.$GSAP.Back.easeInOut.config(5.2), fontSize: "30px" })
-      .to(".smElement", 2, { rotationY: 360 })
-
-    // create a scene
-    const testScene = new this.$ScrollMagic.Scene({
-      triggerElement: ".smElement",
-      triggerHook: 0.7,
-      duration: 300
-    })
-      .addIndicators() // TODO: Remove this line before sending to production
-      .setTween(testTween)
-
-    // Add the scene to the controller
-    this.$ScrollMagic.Controller.addScene(testScene)
+  data() {
+    return {
+      msg: "Hello World!!"
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.dummyDiv {
-  background-color: #aaa;
-  height: 700px;
-}
-.smElement--modified {
-  color: #900;
-  font-weight: bold;
+.hello {
+  color: red;
 }
 </style>
