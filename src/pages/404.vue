02e067044f1470b365f59a23f65470db61fe397d
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-11-16 10:06:45 +0000

    Improve .stylelintrc.json

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,13 @@
 <template>
   <div>404</div>
 </template>
+
+<style lang="scss" scoped>
+.hello {
+  position: relative;
+  z-index: 999;
+  margin: 5px;
+  display: flex;
+  color: $testFromGlobalsVarsScss; // Global Variable
+}
+</style>
--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <h1>FAQ</h1>
+    <h1 class="elo">FAQ</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi,
       eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores
@@ -16,3 +16,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.elo {
+  position: relative;
+  z-index: 999;
+  margin: 5px;
+  display: flex;
+  color: $testFromGlobalsVarsScss; // Global Variable
+}
+</style>
