3505c9d04e7bd6e2919b7fa66b7b53441a67f7a4
”Ozi <31310132+OziOcb@users.noreply.github.com>
2020-01-16 07:44:22 +0000

    #026a - Create a TheAboutMeGallery.vue component file

---
+++ src/components/TheAboutMeGallery.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="theAboutMeGallery">
+    <h2>The About Me Gallery</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, veniam expedita sapiente
+    consequatur minima itaque molestias beatae optio est pariatur saepe rem eum odio recusandae
+    laudantium quasi aut! Mollitia, atque.
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,18 +3,24 @@
     <section>
       <div class="container">
         <h3>Home Page</h3>
-      </div>
-      <div style="height:50px"></div>
+        <div style="height:50px"></div>
 
-      <div style="height:800px"></div>
+        <TheAboutMeGallery />
+
+        <div style="height:800px"></div>
+      </div>
     </section>
   </layout>
 </template>
 
 <script>
+import TheAboutMeGallery from "@/components/TheAboutMeGallery.vue"
 export default {
   metaInfo: {
     title: "Home Page"
+  },
+  components: {
+    TheAboutMeGallery
   }
 }
 </script>
