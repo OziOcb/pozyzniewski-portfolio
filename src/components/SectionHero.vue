be2e495d6076742ef8dba46050668805bd0e8e54
“OziOcb” <pawel_ozi@o2.pl>
2020-01-21 13:30:55 +0000

    #028a - Create and display a SectionHero.vue component file

---
+++ src/components/SectionHero.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hero">
+    <h1>Hero Section Test</h1>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: 100vh;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,21 @@
 <template>
   <layout>
-    <section>
-      <div class="container">
-        <h3>Home Page</h3>
-        <div style="height:50px"></div>
+    <SectionHero />
 
-        <div style="height:800px"></div>
-      </div>
-    </section>
+    <!-- dummy div -->
+    <div style="height:800px;background:#acacac"></div>
   </layout>
 </template>
 
 <script>
+import SectionHero from "@/components/SectionHero.vue"
+
 export default {
   metaInfo: {
     title: "Home Page"
+  },
+  components: {
+    SectionHero
   }
 }
 </script>
