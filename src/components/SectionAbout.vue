dbbc56a7402e7bba58757be0186cb169699da888
”Ozi <31310132+OziOcb@users.noreply.github.com>
2020-01-24 06:51:02 +0000

    #029a - Create and display a SectionAbout.vue file

---
+++ src/components/SectionAbout.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="aboutMe">
+    <h3>About Me</h3>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,19 +2,22 @@
   <layout>
     <main role="main">
       <SectionHero />
+      <SectionAbout />
     </main>
   </layout>
 </template>
 
 <script>
 import SectionHero from "@/components/SectionHero.vue"
+import SectionAbout from "@/components/SectionAbout.vue"
 
 export default {
   metaInfo: {
     title: "Home Page"
   },
   components: {
-    SectionHero
+    SectionHero,
+    SectionAbout
   }
 }
 </script>
