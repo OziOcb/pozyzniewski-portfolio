c3d110a2c6c0a8133057fac4d2738334d53ff28b
”Ozi <31310132+OziOcb@users.noreply.github.com>
2020-01-25 17:09:57 +0000

    #032b - Create a base HTML structure

--- src/components/SectionPortfolio.vue
+++ src/components/SectionPortfolio.vue
@@ -1,7 +1,54 @@
 <template>
-  <h3>Section Portfolio</h3>
+  <section class="portfolio">
+    <div class="portfolio__textWrapper">
+      <h2 class="portfolio__title fixedSize">portfolio</h2>
+      <p class="portfolio__desc">
+        Collection of my recent design and development work. I am constantly working on new projects
+        so this list will soon be expanded.
+      </p>
+    </div>
+
+    <h3 class="portfolio__listTitle">Portfolio Projects List</h3>
+    <ul class="portfolio__list">
+      <li v-for="(project, index) in portfolioProjects" :key="index" class="portfolio__listItem">
+        <PortfolioProject :project-info="project" />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {}
+import PortfolioProject from "@/components/PortfolioProject.vue"
+
+export default {
+  components: {
+    PortfolioProject
+  },
+  data() {
+    return {
+      portfolioProjects: [
+        {
+          year: 2020,
+          title: "POzyzniewski Portfolio",
+          desc: "Design & Development",
+          repoUrl: "https://github.com/OziOcb/pozyzniewski-portfolio",
+          projectUrl: "https://www.twitter.com",
+          colorGradientPrimary: "rgba(111,255,111,0.9)",
+          colorGradientSecondary: "rgba(255,255,255,0.5)",
+          bgImage: "design"
+        },
+        {
+          year: 2019,
+          title: "Dude is this another TODO list?",
+          desc: "Design & Development",
+          repoUrl: "https://github.com/OziOcb/Dude-is-this-another-Todo-List",
+          projectUrl: "https://dude-is-this-another-todolist3.web.app/auth",
+          colorGradientPrimary: "#0091ea",
+          colorGradientSecondary: "rgba(255,255,255,0.5)",
+          bgImage: "default"
+        }
+      ]
+    }
+  }
+}
 </script>
