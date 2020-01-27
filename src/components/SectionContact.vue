2189d0d4b4ff2b258cf9fa93d8633b19fc02ad0e
“OziOcb” <pawel_ozi@o2.pl>
2020-01-27 16:25:38 +0000

    #032b - Create a base HTML structure

--- src/components/SectionContact.vue
+++ src/components/SectionContact.vue
@@ -1,9 +1,23 @@
 <template>
-  <section class="contact">
-    <h3>Contact Me</h3>
+  <section class="contact container">
+    <div class="contact__column">
+      <h2 class="contact__title fixedSize">how to contact me?</h2>
+      <p class="contact__desc">
+        Contact me if you are looking for talented and hard working Front End Developer!
+      </p>
+    </div>
+
+    <div class="contact__column">
+      <ContactMeForm class="contact__contactForm" />
+    </div>
   </section>
 </template>
 
 <script>
-export default {}
+import ContactMeForm from "@/components/ContactMeForm.vue"
+export default {
+  components: {
+    ContactMeForm
+  }
+}
 </script>
