0b4fa7eda8ef77190044ba4846d0c7af3aadc43c
OziOcb <pozyzniewski@yahoo.com>
2022-11-26 12:38:01 +0000

    Finish the youtubePlayer component

--- components/content/youtubePlayer.vue
+++ components/content/youtubePlayer.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="youtubePlayer">
-    <div
-      v-if="!isVideoActive"
-      class="youtubePlayer__cover cover"
-      @click="activateVideoHandler()"
-    >
-      <NuxtImg
-        class="cover__background"
-        :src="`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`"
-      />
-
-      <Icon class="cover__youtubeLogo" name="logos:youtube-icon" size="80" />
-    </div>
-
     <iframe
-      v-else
-      class="youtubePlayer__frame"
-      width="560"
-      height="315"
+      width="100%"
+      height="100%"
       :src="`https://www.youtube.com/embed/${videoId}`"
       :title="`Youtube video player - ${title}`"
       frameborder="0"
@@ -28,43 +13,15 @@
 </template>
 
 <script lang="ts" setup>
-defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
-  videoId: {
-    type: String,
-    required: true,
-  },
-});
-
-const isVideoActive = ref(false);
-
-function activateVideoHandler() {
-  isVideoActive.value = true;
-}
+defineProps<{
+  videoId: string;
+  title: string;
+}>();
 </script>
 
 <style lang="scss" scoped>
 .youtubePlayer {
-  background-color: #ada; // REMOVE_ME: remove when done!
-}
-
-.cover {
-  position: relative;
-  cursor: pointer;
-
-  &__background {
-    display: block;
-    width: 100%;
-  }
-
-  &__youtubeLogo {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
+  height: 52vw;
+  max-height: 655px;
 }
 </style>
