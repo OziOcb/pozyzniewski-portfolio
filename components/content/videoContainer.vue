e8e4eea8592c29a34b7f0a9805ece51f938ab076
OziOcb <pozyzniewski@yahoo.com>
2022-11-26 12:14:25 +0000

    Add the videoPlayer cover

--- components/content/videoContainer.vue
+++ components/content/videoContainer.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="videoPlayer">
+    <div
+      v-if="!isVideoActive"
+      class="videoPlayer__cover cover"
+      @click="activateVideoHandler()"
+    >
+      <NuxtImg
+        class="cover__background"
+        :src="`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`"
+      />
+
+      <Icon class="cover__youtubeLogo" name="logos:youtube-icon" size="80" />
+    </div>
+
     <iframe
+      v-else
+      class="videoPlayer__frame"
       width="560"
       height="315"
       :src="`https://www.youtube.com/embed/${videoId}`"
@@ -12,23 +27,44 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-    videoId: {
-      type: String,
-      required: true,
-    },
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
   },
-};
+  videoId: {
+    type: String,
+    required: true,
+  },
+});
+
+const isVideoActive = ref(false);
+
+function activateVideoHandler() {
+  isVideoActive.value = true;
+}
 </script>
 
 <style lang="scss" scoped>
 .videoPlayer {
   background-color: #ada; // REMOVE_ME: remove when done!
+}
+
+.cover {
+  position: relative;
+  cursor: pointer;
+
+  &__background {
+    display: block;
+    width: 100%;
+  }
+
+  &__youtubeLogo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 }
 </style>
